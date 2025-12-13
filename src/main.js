100b24aa48af98f138bc8817d1af75dd474c835a
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-12-13 08:40:49 +0100

    create project

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import axios from 'axios';
+
+import Swiper, { Navigation, Pagination} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+ 
+import Swal from 'sweetalert2';
+
+import Raty from "raty-js";
+
+import "css-star-rating/css/star-rating.css";
+
+
+
+import './js/header.js';
+import './js/hero.js';
+import './js/pets-list.js';
+import './js/about-us.js';
+import './js/faq.js';
+import './js/success-stories.js';
+import './js/animal-detail.js';
+import './js/order-model.js';
+import './js/footer.js'
+import { Navigation, Pagination } from "swiper/modules";
