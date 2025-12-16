eeacd3151b931e021b566c52dd8f89970af6ffac
seedre <taocripto@gmail.com>
2025-12-16 08:16:11 +0100

    Gotovyj do popravok

---
+++ src/js/about-us.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import '/src/about_us.css';
+
+
+const swiper = new Swiper('.about-swiper', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  loop: false,
+  spaceBetween: 0,
+
+  navigation: {
+    nextEl: '.next-btn',
+    prevEl: '.prev-btn',
+  },
+
+  pagination: {
+    // el: '.swiper-pagination',
+    el: '.about-controls .swiper-pagination',
+    type: 'bullets', 
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 1,
+  },
+  breakpoints: {
+    768: {  // планшет і вище
+      pagination: {
+        dynamicBullets: false, // стаціонарні булети
+      }
+    }
+  }
+}); 
